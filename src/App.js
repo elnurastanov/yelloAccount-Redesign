6cf8a16861c72b4125739e8b8d7d40464f3bb430
logmanCrater <44110804+logmanCrater@users.noreply.github.com>
2020-04-05 23:26:55 +0400

    Salary panel design completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Suspense } from 'react';
 import './App.css';
+import ReactLoading from 'react-loading';
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  ContactsOutlined
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
+// const Dashboard = React.lazy(() => import('./panels/Dashboard'));
+const Salary = React.lazy(() => import('./panel/salary'));
+// const Leader = React.lazy(() => import('./panels/Leader'));
 
 function App() {
+
+  const [collapsed, setCollapsed] = useState(false)
+  function toggle() {
+    setCollapsed(!collapsed)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout style={{ height: "100vh" }}>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/"></Link>
+              <UserOutlined />
+              <span>Dashboard</span>
+            </Menu.Item>
+            <Menu.Item key="2" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Organization"></Link>
+              <VideoCameraOutlined />
+              <span>Təşkilat</span>
+            </Menu.Item>
+            <Menu.Item key="3" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Bank"></Link>
+              <UploadOutlined />
+              <span>Bank</span>
+            </Menu.Item>
+            <Menu.Item key="4" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Treasure"></Link>
+              <UploadOutlined />
+              <span>Xəzinə</span>
+            </Menu.Item>
+            <Menu.Item key="5" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Buy"></Link>
+              <UploadOutlined />
+              <span>Alış</span>
+            </Menu.Item>
+            <Menu.Item key="6" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Sale"></Link>
+              <UploadOutlined />
+              <span>Satış</span>
+            </Menu.Item>
+            <Menu.Item key="7" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Sale"></Link>
+              <UploadOutlined />
+              <span>Satış</span>
+            </Menu.Item>
+            <Menu.Item key="8" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/QMA"></Link>
+              <UploadOutlined />
+              <span>QMA</span>
+            </Menu.Item>
+            <Menu.Item key="9" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Salary"></Link>
+              <ContactsOutlined />
+              <span>Əmək haqqı</span>
+            </Menu.Item>
+            <Menu.Item key="10" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Staff"></Link>
+              <UploadOutlined />
+              <span>Kadrlar</span>
+            </Menu.Item>
+            <Menu.Item key="11" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Monitor"></Link>
+              <UploadOutlined />
+              <span>Monitor</span>
+            </Menu.Item>
+            <Menu.Item key="12" style={{ height: "3rem", lineHeight: "3rem" }}>
+              <Link to="/Leader"></Link>
+              <UploadOutlined />
+              <span>Rəhbər üçün</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Suspense fallback={<ReactLoading type={"bars"} color={"#6236ff"} height={'50px'} width={'50px'} delay={0} style={{margin: 'auto auto'}}/>}>
+              <Switch>
+                <Route exact path="/" component={() => <h1>Dashboard</h1>} />
+                <Route exact path="/Organization" component={() => <h1>Organization</h1>} />
+                <Route exact path="/Bank" component={() => <h1>Bank</h1>} />
+                <Route exact path="/Treasure" component={() => <h1>Treasure</h1>} />
+                <Route exact path="/Buy" component={() => <h1>Buy</h1>} />
+                <Route exact path="/Sale" component={() => <h1>Sale</h1>} />
+                <Route exact path="/ADV" component={() => <h1>ADV</h1>} />
+                <Route exact path="/QMA" component={() => <h1>QMA</h1>} />
+                <Route exact path="/Salary" component={Salary} />
+                <Route exact path="/Staff" component={() => <h1>Staff</h1>} />
+                <Route exact path="/Monitor" component={() => <h1>Monitor</h1>} />
+                <Route exact path="/Leader" component={() => <h1>Leader</h1>} />
+              </Switch>
+            </Suspense>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }
---
+++ src/panel/salary/index.js
@@ -0,0 +1,218 @@
+import React, { useState, useEffect } from 'react'
+import './salary.css'
+import { Form, Select, DatePicker, Input, InputNumber, Progress, Button } from 'antd'
+// import moment from 'moment'
+import { FormOutlined } from '@ant-design/icons'
+
+// Form PROPS
+const layout = {
+    labelCol: { span: 16 },
+    wrapperCol: { span: 16 },
+};
+
+const validateMessages = {
+    required: `Boş saxlamayın!`,
+    types: {
+        email: `is not validate email!`,
+        number: `is not a validate number!`,
+    },
+    number: {
+        range: `must be between min and max`,
+    },
+};
+
+
+
+function Salary() {
+
+    //Salary Components State
+    const [salaryState, setSalaryState] = useState({
+        salaryCompany: undefined,
+        salaryExpenseType: undefined,
+        salaryVendor: undefined,
+        salaryDate: '',
+        salaryStaff: undefined,
+        salaryDestination: '',
+        salaryExpenseChannel: undefined,
+        salaryMonthly: null,
+        salaryPaid: null
+    })
+
+
+    const [form] = Form.useForm();
+    
+    useEffect(() => {
+        form.setFieldsValue({
+            salaryCompany: undefined,
+            salaryExpenseType: undefined,
+            salaryVendor: undefined,
+            salaryDate: '',
+            salaryStaff: undefined,
+            salaryDestination: '',
+            salaryExpenseChannel: undefined,
+        });
+    }, [form])
+
+    // Select Component
+    const options = [{ value: "Yello" }, { value: "YelloAD" }]
+    const options1 = [{ value: "test" }, { value: 'test2' }]
+    const { Option } = Select;
+
+    function ResetForm() {
+        setSalaryState({
+            salaryMonthly: null,
+            salaryPaid: null
+        });
+
+        form.resetFields()
+    }
+
+    function onFinish(event) {
+        setSalaryState({...salaryState,
+            salaryCompany: event.salaryCompany,
+            salaryExpenseType: event.salaryExpenseType,
+            salaryVendor: event.salaryVendor,
+            salaryDate: event.salaryDate,
+            salaryStaff: event.salaryStaff,
+            salaryDestination: event.salaryDestination,
+            salaryExpenseChannel: event.salaryExpenseChannel,
+        })        
+    }
+
+
+
+    return (
+        <section className="Salary">
+            <Form form={form} {...layout} name="Salary__form" onFinish={onFinish} validateMessages={validateMessages} layout='vertical' size='medium' >
+                <div className="Form__customLayout">
+                    <Form.Item name="salaryCompany" label="İcra edən şirkət" rules={[{ required: true }]} >
+                        <Select style={{ width: 200 }}
+                            placeholder="Şirkət">
+                            {
+                                options.map((data, index) => {
+                                    return <Option key={index} value={data.value}>{data.value}</Option>
+                                })
+                            }
+                        </Select>
+                    </Form.Item>
+                    <Form.Item name="salaryVendor" label="İcraçı / Vendor" rules={[{ required: true }]} >
+                        <Select style={{ width: 200 }}
+                            placeholder="İcraçı / Vendor">
+                            {
+                                options.map((data, index) => {
+                                    return <Option key={index} value={data.value}>{data.value}</Option>
+                                })
+                            }
+                        </Select>
+                    </Form.Item>
+                    <Form.Item name="salaryStaff" label="Əməkdaş" rules={[{ required: true }]} >
+                        <Select style={{ width: 200 }}
+                            placeholder="Əməkdaş">
+                            {
+                                options.map((data, index) => {
+                                    return <Option key={index} value={data.value}>{data.value}</Option>
+                                })
+                            }
+                        </Select>
+                    </Form.Item>
+                </div>
+                <div className="Form__customLayout">
+                    <Form.Item name="salaryDate" label="Ödəmə tarixi" rules={[{ required: true }]}>
+                        <DatePicker style={{ width: 200 }} 
+                            format={'DD-MM-YYYY'} />
+                    </Form.Item>
+                    <Form.Item name="salaryExpenseChannel" label="Ödəmə kanalı" rules={[{ required: true }]} >
+                        <Select style={{ width: 200 }}
+                            placeholder="Ödəmə kanalı">
+                            {
+                                options.map((data, index) => {
+                                    return <Option key={index} value={data.value}>{data.value}</Option>
+                                })
+                            }
+                        </Select>
+                    </Form.Item>
+                    <Form.Item name="formExpenseType" label="Xərc maddəsi" rules={[{ required: true }]} >
+                        <Select
+                            style={{ width: 200 }}
+                            placeholder="Xərc maddəsi" >
+                            {
+                                options1.map((data, index) => {
+                                    return <Option key={index} value={data.value}>{data.value}</Option>
+                                })
+                            }
+                        </Select>
+                    </Form.Item>
+                </div>
+                <div className="Form__customLayout">
+                    <Form.Item name="salaryDestination" label="Təyinat" rules={[{ required: true }]} >
+                        <Input placeholder='Açıqlama' 
+                        prefix={<FormOutlined />}
+                        style={{ width: 700 }} />
+                    </Form.Item>
+                </div>
+                <div className="Form__customLayout">
+                    <Form.Item name="salaryMonthly" label="Aylıq əməkhaqqı" rules={[{ required: true }]} >
+                        <InputNumber
+                            formatter={value => `₼ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={value => value.replace(/₼\s?|(,*)/g, '')}
+                            style={{ width: 200 }}
+                            value={salaryState.salaryMonthly}
+                            onChange={(data) => setSalaryState({...salaryState, salaryMonthly : data})}
+
+                        />
+                    </Form.Item>
+                    <Form.Item name="salaryPaid" label="Ödənilən əməkhaqqı" rules={[{ required: true }]} >
+                        <InputNumber
+                            formatter={value => `₼ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={value => value.replace(/₼\s?|(,*)/g, '')}
+                            style={{ width: 200 }}
+                            value={salaryState.salaryPaid}
+                            onChange={(data) => setSalaryState({...salaryState, salaryPaid : data})}
+                        />
+                    </Form.Item>
+                    <Form.Item label="Qalıq əməkhaqqı" rules={[{ required: true }]} >
+                        <InputNumber
+                            formatter={value => `₼ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={value => value.replace(/₼\s?|(,*)/g, '')}
+                            style={{ width: 200 }}
+                            disabled
+                            value={salaryState.salaryMonthly - salaryState.salaryPaid}
+                        />
+                    </Form.Item>
+                </div>
+                <div className="Form__customLayout">
+                    <Progress
+                        strokeColor={{
+                            '0%': '#ff0000',
+                            '50': '#fff800',
+                            '100%': '#00a8ff',
+                        }}
+                        status={
+                            salaryState.salaryPaid / salaryState.salaryMonthly * 100 < 100
+                                ? 'active'
+                                : (salaryState.salaryPaid / salaryState.salaryMonthly * 100 >= 100
+                                    ? 'success'
+                                    : 'active'
+                                )
+                        }
+                        percent={
+                            salaryState.salaryMonthly === null || salaryState.salaryPaid === null
+                                ? 0
+                                : (salaryState.salaryPaid / salaryState.salaryMonthly * 100).toFixed(2)
+                        }
+                    />
+                </div>
+                <div className="Form__footer">
+                    <div className="Form__footer--button">
+                        <Button type="primary" onClick={ResetForm} danger>Sıfırla</Button>
+                    </div>
+                    <div className="Form__footer--button">
+                        <Button type="primary" htmlType="submit">Təsdiqlə</Button>
+                    </div>
+                </div>
+            </Form>
+        </section >
+    )
+}
+
+export default Salary;
